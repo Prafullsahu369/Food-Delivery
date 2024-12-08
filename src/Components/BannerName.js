76e4e6edbf66d807d89b32c70f728b4d27c74cdb
Prafull Sahu <167750545+Prafullsahu369@users.noreply.github.com>
2024-12-08 17:31:08 +0530

    Add files via upload

---
+++ src/Components/BannerName.js
@@ -0,0 +1,36 @@
+// import React from 'react'
+
+// function BannerName({name,discount,link}) {
+//   return (
+// <div className='bannerContent'>
+//     <h3>Hello {name}</h3>
+//     <p>
+//         Get free discount for every <span>${discount}</span> purchase
+//     </p>
+
+//     <a href='{link}'>Learn More </a>
+// </div>
+//   )
+
+// }
+
+// export default BannerName
+
+
+import React from "react";
+
+function BannerName({ name, discount, more }) {
+  const currency = "$";
+  return (
+    <div className="bannerContent">
+      <h3>Hello {name},</h3>
+      <p>
+        Get free discount for every <span>{`${currency}${discount}`}</span>{" "}
+        purchase
+      </p>
+      <a href={more}>Learn More</a>
+    </div>
+  );
+}
+
+export default BannerName;
